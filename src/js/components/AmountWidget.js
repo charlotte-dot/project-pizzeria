85a3be13b58b90b19e50d4ca6c46a56bdfbe31a8
Karolina Smolinska <konto.dev2@gmail.com>
2022-08-21 20:38:23 +0200

    class Booking.js created - 10.4

--- src/js/components/AmountWidget.js
+++ src/js/components/AmountWidget.js
@@ -1,75 +1,75 @@
 import { settings, select } from './../settings.js';
 
 class AmountWidget {
-    constructor(element) {
-      const thisWidget = this;
+  constructor(element) {
+    const thisWidget = this;
 
-      thisWidget.getElements(element);
-      thisWidget.initActions();
-      thisWidget.setValue(
-        thisWidget.input.value || settings.amountWidget.defaultValue
-      );
-    }
+    thisWidget.getElements(element);
+    thisWidget.initActions();
+    thisWidget.setValue(
+      thisWidget.input.value || settings.amountWidget.defaultValue
+    );
+  }
 
-    getElements(element) {
-      const thisWidget = this;
+  getElements(element) {
+    const thisWidget = this;
 
-      thisWidget.element = element;
-      thisWidget.input = thisWidget.element.querySelector(
-        select.widgets.amount.input
-      );
-      thisWidget.linkDecrease = thisWidget.element.querySelector(
-        select.widgets.amount.linkDecrease
-      );
-      thisWidget.linkIncrease = thisWidget.element.querySelector(
-        select.widgets.amount.linkIncrease
-      );
-    }
-    setValue(value) {
-      const thisWidget = this;
+    thisWidget.element = element;
+    thisWidget.input = thisWidget.element.querySelector(
+      select.widgets.amount.input
+    );
+    thisWidget.linkDecrease = thisWidget.element.querySelector(
+      select.widgets.amount.linkDecrease
+    );
+    thisWidget.linkIncrease = thisWidget.element.querySelector(
+      select.widgets.amount.linkIncrease
+    );
+  }
+  setValue(value) {
+    const thisWidget = this;
 
-      const newValue = parseInt(value); // "aa" -> NaN
+    const newValue = parseInt(value); // "aa" -> NaN
 
-      /* TODO: Add validation */
-      if (thisWidget.value !== newValue && !isNaN(newValue)) {
-        if (newValue < settings.amountWidget.defaultMin) {
-          thisWidget.value = settings.amountWidget.defaultMin;
-        } else if (newValue > settings.amountWidget.defaultMax) {
-          thisWidget.value = settings.amountWidget.defaultMax;
-        } else {
-          thisWidget.value = newValue;
-        }
-
-        thisWidget.announce();
+    /* TODO: Add validation */
+    if (thisWidget.value !== newValue && !isNaN(newValue)) {
+      if (newValue < settings.amountWidget.defaultMin) {
+        thisWidget.value = settings.amountWidget.defaultMin;
+      } else if (newValue > settings.amountWidget.defaultMax) {
+        thisWidget.value = settings.amountWidget.defaultMax;
+      } else {
+        thisWidget.value = newValue;
       }
 
-      thisWidget.input.value = thisWidget.value;
+      thisWidget.announce();
     }
 
-    announce() {
-      const thisWidget = this;
-
-      const event = new CustomEvent('updated', {
-        bubbles: true,
-      });
-      thisWidget.element.dispatchEvent(event);
-    }
-
-    initActions() {
-      const thisWidget = this;
-      thisWidget.input.addEventListener('change', function (e) {
-        thisWidget.setValue(e.target.value);
-      });
-
-      thisWidget.linkDecrease.addEventListener('click', function (e) {
-        e.preventDefault();
-        thisWidget.setValue(thisWidget.value - 1);
-      });
-
-      thisWidget.linkIncrease.addEventListener('click', function (e) {
-        e.preventDefault();
-        thisWidget.setValue(thisWidget.value + 1);
-      });
-    }
+    thisWidget.input.value = thisWidget.value;
   }
-  export default AmountWidget;
+
+  announce() {
+    const thisWidget = this;
+
+    const event = new CustomEvent('updated', {
+      bubbles: true,
+    });
+    thisWidget.element.dispatchEvent(event);
+  }
+
+  initActions() {
+    const thisWidget = this;
+    thisWidget.input.addEventListener('change', function (e) {
+      thisWidget.setValue(e.target.value);
+    });
+
+    thisWidget.linkDecrease.addEventListener('click', function (e) {
+      e.preventDefault();
+      thisWidget.setValue(thisWidget.value - 1);
+    });
+
+    thisWidget.linkIncrease.addEventListener('click', function (e) {
+      e.preventDefault();
+      thisWidget.setValue(thisWidget.value + 1);
+    });
+  }
+}
+export default AmountWidget;
--- src/js/settings.js
+++ src/js/settings.js
@@ -1,147 +1,147 @@
 export const select = {
-    templateOf: {
-      menuProduct: '#template-menu-product',
-      cartProduct: '#template-cart-product', // CODE ADDED
-      bookingWidget: '#template-booking-widget',
+  templateOf: {
+    menuProduct: '#template-menu-product',
+    cartProduct: '#template-cart-product', // CODE ADDED
+    bookingWidget: '#template-booking-widget',
+  },
+  containerOf: {
+    menu: '#product-list',
+    cart: '#cart',
+    pages: '#pages',
+    booking: '.booking-wrapper',
+  },
+  all: {
+    menuProducts: '#product-list > .product',
+    menuProductsActive: '#product-list > .product.active',
+    formInputs: 'input, select',
+  },
+  menuProduct: {
+    clickable: '.product__header',
+    form: '.product__order',
+    priceElem: '.product__total-price .price',
+    imageWrapper: '.product__images',
+    amountWidget: '.widget-amount',
+    cartButton: '[href="#add-to-cart"]',
+  },
+  widgets: {
+    amount: {
+      input: 'input.amount', // CODE CHANGED
+      linkDecrease: 'a[href="#less"]',
+      linkIncrease: 'a[href="#more"]',
     },
-    containerOf: {
-      menu: '#product-list',
-      cart: '#cart',
-      pages: '#pages',
-      booking: '.booking-wrapper',
+  },
+  // CODE ADDED START
+  cart: {
+    productList: '.cart__order-summary',
+    toggleTrigger: '.cart__summary',
+    totalNumber: `.cart__total-number`,
+    totalPrice:
+      '.cart__total-price strong, .cart__order-total .cart__order-price-sum strong',
+    subtotalPrice: '.cart__order-subtotal .cart__order-price-sum strong',
+    deliveryFee: '.cart__order-delivery .cart__order-price-sum strong',
+    form: '.cart__order',
+    formSubmit: '.cart__order [type="submit"]',
+    phone: '[name="phone"]',
+    address: '[name="address"]',
+  },
+  cartProduct: {
+    amountWidget: '.widget-amount',
+    price: '.cart__product-price',
+    edit: '[href="#edit"]',
+    remove: '[href="#remove"]',
+  },
+  timeWidgets: {
+    datePicker: {
+      wrapper: '.date-picker',
+      input: 'input[name="date"]',
     },
-    all: {
-      menuProducts: '#product-list > .product',
-      menuProductsActive: '#product-list > .product.active',
-      formInputs: 'input, select',
+    hourPicker: {
+      wrapper: '.hour-picker',
+      input: 'input[type="range"]',
+      output: '.output',
     },
-    menuProduct: {
-      clickable: '.product__header',
-      form: '.product__order',
-      priceElem: '.product__total-price .price',
-      imageWrapper: '.product__images',
-      amountWidget: '.widget-amount',
-      cartButton: '[href="#add-to-cart"]',
-    },
-    widgets: {
-      amount: {
-        input: 'input.amount', // CODE CHANGED
-        linkDecrease: 'a[href="#less"]',
-        linkIncrease: 'a[href="#more"]',
-      },
-    },
-    // CODE ADDED START
-    cart: {
-      productList: '.cart__order-summary',
-      toggleTrigger: '.cart__summary',
-      totalNumber: `.cart__total-number`,
-      totalPrice:
-        '.cart__total-price strong, .cart__order-total .cart__order-price-sum strong',
-      subtotalPrice: '.cart__order-subtotal .cart__order-price-sum strong',
-      deliveryFee: '.cart__order-delivery .cart__order-price-sum strong',
-      form: '.cart__order',
-      formSubmit: '.cart__order [type="submit"]',
-      phone: '[name="phone"]',
-      address: '[name="address"]',
-    },
-    cartProduct: {
-      amountWidget: '.widget-amount',
-      price: '.cart__product-price',
-      edit: '[href="#edit"]',
-      remove: '[href="#remove"]',
-    },
-    widgets: {
-        datePicker: {
-            wrapper: '.date-picker',
-            input: `input[name="date"]`,
-        },
-        hourPicker: {
-            wrapper: '.hour-picker',
-            input: 'input[type="range"]',
-            output: '.output',
-        },
-    },
-    booking: {
-        peopleAmount: '.people-amount',
-        hoursAmount: '.hours-amount',
-        tables: '.floor-plan .table',
-    },
-    nav: {
-        links: '.main-nav a',
-    },
-  };
+  },
+  booking: {
+    peopleAmount: '.people-amount',
+    hoursAmount: '.hours-amount',
+    tables: '.floor-plan .table',
+  },
+  nav: {
+    links: '.main-nav a',
+  },
+};
 
 export const classNames = {
-        menuProduct: {
-          wrapperActive: 'active',
-          imageVisible: 'active',
-        },
-        // CODE ADDED START
-        cart: {
-          wrapperActive: 'active',
-        },
-        booking: {
-          loading: 'loading',
-          tableBooked: 'booked',
-      },
-        nav: {
-          active: 'active',
-      },
-        pages: {
-          active: 'active',
-      }
-        // CODE ADDED END
-  };
-    
+  menuProduct: {
+    wrapperActive: 'active',
+    imageVisible: 'active',
+  },
+  // CODE ADDED START
+  cart: {
+    wrapperActive: 'active',
+  },
+  booking: {
+    loading: 'loading',
+    tableBooked: 'booked',
+  },
+  nav: {
+    active: 'active',
+  },
+  pages: {
+    active: 'active',
+  },
+  // CODE ADDED END
+};
+
 export const settings = {
-        amountWidget: {
-          defaultValue: 1,
-          defaultMin: 1,
-          defaultMax: 9,
-        }, // CODE CHANGED
-        // CODE ADDED START
-        cart: {
-          defaultDeliveryFee: 20,
-        },
-        // CODE ADDED END
-        db: {
-          url: '//localhost:3131',
-          products: 'products',
-          orders: 'orders',
-        },
-        hours: {
-          open: 12,
-          close: 24,
-      },
-      datePicker: {
-          maxDaysInFuture: 14,
-      },
-      booking: {
-          tableIdAttribute: 'data-table',
-      },
-      db: {
-          url: '//localhost:3131',
-          product: 'product',
-          order: 'order',
-          booking: 'booking',
-          event: 'event',
-          dateStartParamKey: 'date_gte',
-          dateEndParamKey: 'date_lte',
-          notRepeatParam: 'repeat=false',
-          repeatParam: 'repeat_ne=false',
-      },
-    },
-  };
+  amountWidget: {
+    defaultValue: 1,
+    defaultMin: 1,
+    defaultMax: 9,
+  }, // CODE CHANGED
+  // CODE ADDED START
+  cart: {
+    defaultDeliveryFee: 20,
+  },
+  // CODE ADDED END
+  db: {
+    url: '//localhost:3131',
+    products: 'products',
+    orders: 'orders',
+  },
+  hours: {
+    open: 12,
+    close: 24,
+  },
+  datePicker: {
+    maxDaysInFuture: 14,
+  },
+  booking: {
+    tableIdAttribute: 'data-table',
+  },
+  db: {
+    url: '//localhost:3131',
+    product: 'product',
+    order: 'order',
+    booking: 'booking',
+    event: 'event',
+    dateStartParamKey: 'date_gte',
+    dateEndParamKey: 'date_lte',
+    notRepeatParam: 'repeat=false',
+    repeatParam: 'repeat_ne=false',
+  },
+};
 
 export const templates = {
-        menuProduct: Handlebars.compile(
-          document.querySelector(select.templateOf.menuProduct).innerHTML
-        ),
-        // CODE ADDED START
-        cartProduct: Handlebars.compile(
-          document.querySelector(select.templateOf.cartProduct).innerHTML
-        ),
-        bookingWidget: Handlebars.compile(document.querySelector(select.templateOf.bookingWidget).innerHTML),
-        // CODE ADDED END
-      };
-    },
+  menuProduct: Handlebars.compile(
+    document.querySelector(select.templateOf.menuProduct).innerHTML
+  ),
+  // CODE ADDED START
+  cartProduct: Handlebars.compile(
+    document.querySelector(select.templateOf.cartProduct).innerHTML
+  ),
+  bookingWidget: Handlebars.compile(
+    document.querySelector(select.templateOf.bookingWidget).innerHTML
+  ),
+  // CODE ADDED END
+};
